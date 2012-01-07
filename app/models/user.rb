9f3f7a55280ad843422f2b28ceb019436606db5b
Your Name <fmemon100@yahoo.com>
2012-01-07 12:22:23 -0500

    Added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   
   has_many :articles, :order => "published_at DESC, title ASC", :dependent => :destroy
   has_one :profile
+  
+  has_many :replies, :through => :articles, :source => :comments
+  
 end
