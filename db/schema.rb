e3591297c072468b029ebaef1685693f1c9f4285
Your Name <fmemon100@yahoo.com>
2012-01-07 10:57:05 -0500

    Upto page 84 sofar good user and profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120107153740) do
+ActiveRecord::Schema.define(:version => 20120107155241) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(:version => 20120107153740) do
     t.string   "excerpt"
     t.text     "body"
     t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "color"
+    t.string   "twitter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
