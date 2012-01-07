890551a398cee78a78004216793d33c2c14f748b
Your Name <fmemon100@yahoo.com>
2012-01-07 16:47:13 -0500

    Added the comment observer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,22 @@
 class Comment < ActiveRecord::Base
 
-  belongs_to :articles
+  belongs_to :article
+  
+  validates :name, :email, :body, :presence => true
+  validate :article_should_be_published
+  
+  # after_create :email_article_author
+  
+  def article_should_be_published
+    errors.add(:article_id, " is not published yet") if article && !article.published?
+  end
+  
+#   def after_create
+#     puts "We will notify the author in Chapter 9"
+#   end
+
+  #def email_article_author
+  #  puts "We will notify #{author.user.email} in Chatper 9""
+  #end
+
 end
